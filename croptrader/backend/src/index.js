2e04b62f48156541a9e1112922b0da8afbff5038
Steven Keezer <stevenkeezer@Stevens-MacBook.local>
2020-07-17 21:01:09 -0700

    initial commit

---
+++ croptrader/backend/src/index.js
@@ -0,0 +1,47 @@
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+server.express.use(cookieParser());
+//todo use express middleware to populate current user
+
+//decond the jwt so we can get user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
+
+server.express.use(async (req, res, next) => {
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    {
+      where: { id: req.userId },
+    },
+    `{id, permissions, email, name}`
+  );
+  req.user = user;
+  next();
+});
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  (deets) => {
+    console.log(
+      `server is up and running on port http:/localhost:${deets.port}`
+    );
+  }
+);
